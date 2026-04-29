0e06496aecd405b068f909541fe2f03781fb2c7a
joshuatochinwachi <joshuatochinwachi@gmail.com>
2026-04-29 07:28:14 +0100

    revamped

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Building a FastAPI application with Python
 
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Response
 
 # Create an instance of the FastAPI application
 app = FastAPI()
@@ -22,4 +23,16 @@ def about():
 @app.get("/csv")
 def csv():
     csv_content = "name,age,city\nAlice,30,New York\nBob,25,Los Angeles\nCharlie,35,Chicago"
-    return {"csv_data": csv_content}
+    return Response(content=csv_content, media_type="text/plain")
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False
+    )
